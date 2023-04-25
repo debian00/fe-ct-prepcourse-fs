0dbd840c59ebb33206164242d0caea19f4efbfff
debian00 <debianstark@gmail.com>
2023-04-25 03:10:53 -0600

    M06

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -144,11 +144,10 @@ function agregarMetodoCalculoDescuento(objetoProducto) {
    // PorcentajeDeDescuento ---> 0.2
    // Precio final ---> 8
    // Tu código:
-objetoProducto.calcularPrecioDescuento = function (paras) {
-   return objetoProducto.precio * objetoProducto.porcentajeDeDescuento;
+objetoProducto.calcularPrecioDescuento = function () {
+   return this.precio - this.precio * this.porcentajeDeDescuento;
    }
-
-
+   return objetoProducto;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
